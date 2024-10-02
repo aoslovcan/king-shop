dff100e38981538ab52e16440af4cbc20d2c2625
aoslovcan <tonkica1605.o@gmail.com>
2024-10-02 17:44:38 +0200

    add colors, sizes, fonts

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 export const HomePage = () => {
-  return <h3>This is home page</h3>;
+  return <h3 className="font-primary text-3xl font-bold">This is home page</h3>;
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,45 @@
 export default {
   content: ['index.html', './src/**/*.{html, js,ts,jsx,tsx}'],
   theme: {
+    screens: {
+      xs: '250px',
+      sm: '540px',
+      md: '768px',
+      // => @media (min-width: 768px) { ... }
+
+      lg: '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      xl: '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px'
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {
+      fontFamily: {
+        primary: ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        color: {
+          primaryText: "#1E1E1E",
+        },
+        background: {
+            primary: "#C1DCDC",
+        }
+      },
+      fontSize: {
+        '3xs': '8px',
+        '2xs': '10px',
+        xxs: '0.625rem', //10px
+        xs: '0.75rem', //12px,
+        sm: '0.875rem', //14px,
+        md: '1rem', //16px,
+        lg: '1.125rem', //18px,
+        xl: '1.25rem', //20px,
+        '2xl': '1.375rem', //22px,
+        '3xl': '1.5rem' //24px,
+      }
     }
   },
   plugins: []
