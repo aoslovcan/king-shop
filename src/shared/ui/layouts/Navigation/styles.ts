69cb06d70cb2faed8e788679a2e954cd38107dfb
aoslovcan <tonkica1605.o@gmail.com>
2024-10-04 16:28:15 +0200

    add page layout guide

--- src/shared/ui/layouts/Navigation/styles.ts
+++ src/shared/ui/layouts/Navigation/styles.ts
@@ -1,4 +1,4 @@
-export const mainNavigationContainer = 'fixed w-full h-[60px] shadow mb-16 bg-white';
+export const mainNavigationContainer = 'fixed w-full h-[60px] shadow mb-16 bg-white z-50';
 export const navigation = 'h-full px-4 flex flex-row items-center justify-between';
 
 export const navigationMenu = 'flex flex-row gap-8 items-center';
--- src/shared/ui/layouts/PageLayout/PageLayout.tsx
+++ src/shared/ui/layouts/PageLayout/PageLayout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {Navigation} from "../Navigation/Navigation.tsx";
 import {Header} from "../Header/Header.tsx";
+import {PageSideBar} from "../PageSideBar/PageSideBar.tsx";
 interface PageLayoutProps {
     children : React.ReactNode
 }
@@ -9,16 +10,18 @@ export const PageLayout = ({children} : PageLayoutProps) => {
         <>
             <Navigation/>
             <div className="flex flex-col p-6 pt-16">
-               <Header/>
+                <Header/>
                 <div className="flex flex-row mt-[60px] h-full gap-4">
                     <div className="w-1/5 h-[100vh]">
-                        This is sidebar
+                        <PageSideBar/>
                     </div>
                     <div className="w-4/5 h-full">
                         {children}
                     </div>
                 </div>
             </div>
+            <div className="bg-background-secondary h-[10vh] w-full items-center justify-center"></div>
+
         </>
 
     )
