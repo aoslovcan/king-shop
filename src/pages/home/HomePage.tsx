991c6081d30e005f908c4f5830522a074912c4a4
aoslovcan <tonkica1605.o@gmail.com>
2024-10-04 15:07:27 +0200

    clean the home page

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,8 +1,3 @@
-import React from 'react';
-import {Button, Loading, ProductCard, Text} from "shared/ui";
-import {ArrowRightIcon, CloseIcon} from "../../shared/assets/icons";
-import {trimTextToFullSentence} from "shared/utils";
-
 export const HomePage = () => {
 
     return "Hello"
