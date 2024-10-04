fb98fecd0000b5bee201c970bc4072f49e484c37
aoslovcan <tonkica1605.o@gmail.com>
2024-10-04 15:06:42 +0200

    add page style layout

--- src/shared/ui/layouts/PageLayout/PageLayout.tsx
+++ src/shared/ui/layouts/PageLayout/PageLayout.tsx
@@ -8,11 +8,11 @@ export const PageLayout = ({children} : PageLayoutProps) => {
             <div>Navigation</div>
             <div className="flex flex-col p-6">
                 <div>Header</div>
-                <div className="flex flex-row mt-[60px]">
-                    <div className="w-1/5">
+                <div className="flex flex-row mt-[60px] h-full gap-4">
+                    <div className="w-1/5 h-[100vh]">
                         This is sidebar
                     </div>
-                    <div className="w-4/5">
+                    <div className="w-4/5 h-full">
                         {children}
                     </div>
                 </div>
