34ea246c206e1840cb0187919daebbd5919e1dd8
aoslovcan <tonkica1605.o@gmail.com>
2024-10-05 21:03:52 +0200

    add number of items in cart

--- src/shared/ui/layouts/Navigation/Navigation.tsx
+++ src/shared/ui/layouts/Navigation/Navigation.tsx
@@ -3,22 +3,31 @@ import {Link} from 'react-router-dom';
 import React, {useState} from 'react';
 import {CartIcon, UserIcon} from "shared/assets/icons";
 import {SideBar} from "../SideBar/SideBar.tsx";
-import {Cart} from "../../../../widget/Cart";
+import {Cart, useCart} from "../../../../widget/Cart";
 
 export const Navigation = () => {
 
     const [isOpenCart, setOpenCart] = useState(false)
 
+    const {cartData} = useCart()
+
     return (
         <>
-
-
             <div className={mainNavigationContainer}>
                 <div className={navigation}>
                     <div className="text-background-secondary text-3xl">KingShop</div>
                     <ul className={navigationMenu}>
                         <li className={navigationMenuItem}>
-                            <span className="cursor-pointer" onClick={() => setOpenCart(true)}><CartIcon/></span>
+                            <span className="relative cursor-pointer" onClick={() => setOpenCart(true)}>
+                                 {cartData.length > 0 && (
+                                     <span
+                                         className="absolute top-[-4px] right-[-8px] bg-red-500 text-white text-xs font-bold rounded-full w-4 h-4 flex items-center justify-center">
+                {cartData.length}
+            </span>
+                                 )}
+                                <CartIcon/>
+
+                            </span>
                         </li>
                         <li className={navigationMenuItem}>
                             <Link to="/about">
@@ -36,7 +45,7 @@ export const Navigation = () => {
                     isOpen={isOpenCart} handleClose={() => setOpenCart(false)}>
 
                     <Cart/>
-            </SideBar>)}
+                </SideBar>)}
         </>
 
     );
